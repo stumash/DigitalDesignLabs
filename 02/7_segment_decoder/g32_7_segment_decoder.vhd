111fefee9ff0b52eae58a771d345a147875c1eac
Stuart Mashaal <stumash1@gmail.com>
2017-10-20 23:52:19 -0400

    reverse 7_segment output bits

--- 02/7_segment_decoder/g32_7_segment_decoder.vhd
+++ 02/7_segment_decoder/g32_7_segment_decoder.vhd
@@ -43,38 +43,38 @@ begin
         -- mode=0
         -- hex digits (0, 1, ..., 9, A, B, ...)
         -- TODO: reverse all outputs!
-        "0000001" when "00000", -- "0" when mode=0, code=0
-        "1001111" when "00001", -- "1" when mode=0, code=1
-        "0010010" when "00010", -- "2" when mode=0, code=2
-        "0000110" when "00011", -- "3" when mode=0, code=3
-        "1001100" when "00100", -- "4" when mode=0, code=4
-        "0100100" when "00101", -- "5" when mode=0, code=5
-        "0100000" when "00110", -- "6" when mode=0, code=6
-        "0001111" when "00111", -- "7" when mode=0, code=7
+        "1000000" when "00000", -- "0" when mode=0, code=0
+        "1111001" when "00001", -- "1" when mode=0, code=1
+        "0100100" when "00010", -- "2" when mode=0, code=2
+        "0110000" when "00011", -- "3" when mode=0, code=3
+        "0011001" when "00100", -- "4" when mode=0, code=4
+        "0010010" when "00101", -- "5" when mode=0, code=5
+        "0000010" when "00110", -- "6" when mode=0, code=6
+        "1111000" when "00111", -- "7" when mode=0, code=7
         "0000000" when "01000", -- "8" when mode=0, code=8
-        "0000100" when "01001", -- "9" when mode=0, code=9
+        "0010000" when "01001", -- "9" when mode=0, code=9
         "0001000" when "01010", -- "A" when mode=0, code=10
-        "1100000" when "01011", -- "b" when mode=0, code=11
-        "0110001" when "01100", -- "C" when mode=0, code=12
-        "1000010" when "01101", -- "d" when mode=0, code=13
-        "0110000" when "01110", -- "E" when mode=0, code=14
-        "0111000" when "01111", -- "F" when mode=0, code=15
+        "0000011" when "01011", -- "b" when mode=0, code=11
+        "1000110" when "01100", -- "C" when mode=0, code=12
+        "0100001" when "01101", -- "d" when mode=0, code=13
+        "0000110" when "01110", -- "E" when mode=0, code=14
+        "0001110" when "01111", -- "F" when mode=0, code=15
         -- mode=1
         -- card ranks (A, 2, ...,     J, Q, K)
         "0001000" when "10000", -- "A" when mode=1, code=0
-        "0010010" when "10001", -- "2" when mode=1, code=1
-        "0000110" when "10010", -- "3" when mode=1, code=2
-        "1001100" when "10011", -- "4" when mode=1, code=3
-        "0100100" when "10100", -- "5" when mode=1, code=4
-        "0100000" when "10101", -- "6" when mode=1, code=5
-        "0001111" when "10110", -- "7" when mode=1, code=6
+        "0100010" when "10001", -- "2" when mode=1, code=1
+        "0110000" when "10010", -- "3" when mode=1, code=2
+        "0011001" when "10011", -- "4" when mode=1, code=3
+        "0010010" when "10100", -- "5" when mode=1, code=4
+        "0000010" when "10101", -- "6" when mode=1, code=5
+        "1111000" when "10110", -- "7" when mode=1, code=6
         "0000000" when "10111", -- "8" when mode=1, code=7
-        "0000100" when "11000", -- "9" when mode=1, code=8
-        "0000001" when "11001", -- "0" when mode=1, code=9
-        "1000011" when "11010", -- "J" when mode=1, code=10
-        "1100010" when "11011", -- "q" when mode=1, code=11
-        "1001000" when "11100", -- "K" when mode=1, code=12
-        "1111110" when "11101", -- "-" when mode=1, code=13
-        "1111110" when "11110", -- "-" when mode=1, code=14
-        "1111110" when "11111", -- "-" when mode=1, code=15
+        "0010000" when "11000", -- "9" when mode=1, code=8
+        "1000000" when "11001", -- "0" when mode=1, code=9
+        "1100001" when "11010", -- "J" when mode=1, code=10
+        "0100011" when "11011", -- "q" when mode=1, code=11
+        "0001001" when "11100", -- "K" when mode=1, code=12
+        "0111111" when "11101", -- "-" when mode=1, code=13
+        "0111111" when "11110", -- "-" when mode=1, code=14
+        "0111111" when "11111", -- "-" when mode=1, code=15
 end architecture sim;
